72f40f356d3a97ddc0418dc366f68b04d2f77e0c
Mykhailo Didur <mikhaildidur2003@gmail.com>
2023-04-16 18:20:42 +0300

    Initialize the project
    Signed-off-by: Mykhailo Didur <mikhaildidur2003@gmail.com>

---
+++ include/cw_utils.hpp
@@ -0,0 +1,4 @@
+#ifndef CW_UTILS_HPP_
+#define CW_UTILS_HPP_
+
+#endif // CW_UTILS_HPP_
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "cw_utils.hpp"
+
+using namespace std;
+
+int main() {
+	cout << "hello world" << endl;
+}
